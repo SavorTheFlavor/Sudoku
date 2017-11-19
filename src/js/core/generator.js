397a12bc6f78bfce1c4a7716f679066fa95c83fe
SavorTheFlavor2 <2500364530@qq.com>
2017-11-19 18:59:52 +0800

    随机生成数独解决方案...

---
+++ src/js/core/generator.js
@@ -0,0 +1,71 @@
+//生成数独解决方案
+const Toolkit = require("./toolkit")
+
+class Generator{
+	//新语法啊，不习惯
+	generate(){
+		while(!this.internalGenerate()){
+			
+		}
+	}
+
+	internalGenerate(){
+		this.matrix = Toolkit.matrix.makeMatrix();
+		this.orders = Toolkit.matrix.makeMatrix()
+					.map(row => row.map((v,i) => i))
+					.map(row => Toolkit.matrix.shuffle(row));//对每一行进行打乱
+
+		for (var n = 1; n <= 9; n++) {
+			if(!this.fillNumber(n)){
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+
+	//填数字n
+	fillNumber(n){
+		return this.fillRow(n,0);
+	}
+
+	fillRow(n,rowIndex){
+		if(rowIndex > 8){
+			return true;
+		}
+		const row = this.matrix[rowIndex];
+		//对rowIndex行的随机序列
+		const orders = this.orders[rowIndex];
+		for (let i = 0; i < 9; i++) {
+			const colIndex = orders[i];
+			//已填过，跳过
+			if(row[colIndex]){
+				continue;
+			}
+
+			//是否可填
+			//checkFillable()
+			if(!Toolkit.matrix.checkFillable(this.matrix,n,rowIndex,colIndex)){
+				continue;
+			}
+
+			row[colIndex] = n;
+
+			//对于数字n，填下一行
+			if(!this.fillRow(n, rowIndex + 1)){
+				row[colIndex] = 0;
+				continue;
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+}
+
+const generator = new Generator();
+generator.generate();
+console.log(generator.matrix);
--- src/js/core/toolkit.js
+++ src/js/core/toolkit.js
@@ -18,6 +18,27 @@ const matrixToolkit={
 			[arr[i],arr[j]] = [arr[j],arr[i]];
 		}
 		return arr;
+	},
+	/**
+		检查目标位置是否可填
+	*/
+	checkFillable(matrix,n,rowIndex,colIndex){
+		const row = matrix[rowIndex];
+		const column = this.makeRow()
+					.map((v,i) => matrix[i][colIndex]);
+		//这又是什么语法
+		//只需要取出boxIndex
+		const { boxIndex } = boxTookit.convertToBoxIndex(rowIndex,colIndex);
+		//取出一个宫
+		const box = boxTookit.getBoxCells(matrix,boxIndex);
+		for (var i = 0; i < 9; i++) {
+			//检查行列宫是否已存在n
+			if(row[i] === n || column[i] === n
+				|| box[i] === n){
+				return false;
+			}
+		}
+		return true;
 	}
 };
 
@@ -25,7 +46,33 @@ const matrixToolkit={
 	宫坐标系工具
 */
 const boxTookit = {
+	//boxIndex:第几宫
+	//cellIndex:在宫里的第几格
+	convertToBoxIndex(rowIndex,colIndex){
+		return {
+			boxIndex:Math.floor(rowIndex/3)*3 + Math.floor(colIndex/3),
+			cellIndex: (rowIndex%3) * 3 + colIndex%3
+		};
+	},
 
+	//转回在整个矩阵中的rowIndex和colIndex
+	convertFromBoxIndex(boxIndex,cellIndex){
+		return {
+			rowIndex: Math.floor(boxIndex / 3)*3 + Math.floor(cellIndex/3),
+			colIndex: (boxIndex % 3)*3 + colIndex%3
+		};
+	},
+	getBoxCells(matrix,boxIndex){
+		const startRowIndex = Math.floor(boxIndex/3)*3;
+		const startColIndex = (boxIndex%3)*3;
+		const result = [];
+		for (let i = 0; i < 9; i++) {
+			const rowIndex = startRowIndex + Math.floor(i/3);
+			const colIndex = startColIndex + i%3;
+			result.push(matrix[rowIndex][colIndex]);
+		}
+		return result;
+	}
 
 };
 
